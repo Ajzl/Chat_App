a39e7900cf792296959aa5eda144a77df93f0225
Ajsal K <ajsalkannayil88@gmail.com>
2023-11-17 00:37:39 +0530

    Home_Screen Design

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -12,19 +12,53 @@ class _Home_ScreenState extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("Chats"),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            colors: [Color(0xFF58427C), Color(0xFF746CC0)],
-          )),
-        ),
-      ),
-      body: Container(
-          // color: Color(),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text("Chats"),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Color(0xFF58427C), Color(0xFF746CC0)],
+            )),
           ),
-    );
+        ),
+        body: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (context, index) => Card(
+            color: Color(0xFF746CC0).withOpacity(.2),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 3,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(15),
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                        child: Icon(CupertinoIcons.person), radius: 28),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Name",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "Last Message",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
